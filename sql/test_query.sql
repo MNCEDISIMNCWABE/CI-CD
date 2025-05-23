df3e602693b564676ffd7aaeb362526dabe81ed2
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2025-05-23 21:50:34 +0200

    Update test_query.sql

--- sql/test_query.sql
+++ sql/test_query.sql
@@ -1,7 +1,9 @@
--- Overwrites table each run (simplest solution)
-CREATE OR REPLACE TABLE `hallowed-span-459710-s1.test_clustering.cameroon_user_counts` AS
+CREATE OR REPLACE TABLE 
+    `hallowed-span-459710-s1.test_clustering.cameroon_user_counts` 
+AS
 SELECT
     COUNT(DISTINCT user_id) AS user_count
 FROM
     `hallowed-span-459710-s1.test_clustering.user-engagement`
-WHERE country = 'Cameroon'
+WHERE 
+    country = 'Cameroon'
